5da86bac829e0c558f9670a29cdd6dfa5ea1b7d3
Shuwen He <1201220707@pku.edu.cn>
2023-06-15 15:57:05 +0800

    fix:cpp

--- cpp/app.cpp
+++ cpp/app.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// 高斯求和公式求和1+2+3+...+100
 int gauss() {
     int sum = 0;
     sum = (1+100)*100/2;
@@ -9,6 +10,7 @@ int gauss() {
     return 0;
 }
 
+// for循环求和1+2+3+...+100
 int forSum(){
     int sum = 0;
     for (int i = 1; i <= 100; i++) {
