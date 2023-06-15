bf6d66f5f34b42850c0c1c2147bd7ad5846dd4a2
Shuwen He <1201220707@pku.edu.cn>
2023-06-15 15:37:09 +0800

    fix:app

--- cpp/for/app.cpp
+++ cpp/for/app.cpp
@@ -2,15 +2,13 @@
 
 using namespace std;
 
-// gauss 
 int gauss() {
     int sum = 0;
     sum = (1+100)*100/2;
-    std::cout << "gauss sum=" << sum << std::endl;
+    cout << "gauss sum=" << sum << endl;
     return 0;
 }
 
-// forSum
 int forSum(){
     int sum = 0;
     for (int i = 1; i <= 100; i++) {
@@ -19,7 +17,6 @@ int forSum(){
     cout << "for sum=" << sum << endl;
     return 0;
 }
-
 
 int main() {
     forSum();
