7d3af81f47e4039c08878e08f769641a430f6e9d
Shuwen He <1201220707@pku.edu.cn>
2023-06-18 17:40:49 +0800

    fix: type

--- cpp/csp.cpp
+++ cpp/csp.cpp
@@ -19,6 +19,65 @@ int forSum(){
     cout << "for sum=" << sum << endl;
     return 0;
 }
+// linux命令
+// 文件命令
+
+// C++
+// 数据类型
+int intType(){
+	// int 整型
+	int num = 10;
+	cout<<"num = "<<num<<endl;
+	return 0;
+}
+
+int shortType(){
+	short short_num = 20;
+	cout<<"short_num = "<<short_num<<endl;
+	return 0;
+}
+
+int longType(){
+	long long_num = 1234567890;
+	cout<<"long_num = "<<long_num<<endl;
+	return 0;
+}
+
+int longLongType(){
+	long long long_long_num =9876543210;
+	cout<<"longLong_num ="<<long_long_num<<endl;
+	return 0;
+}	
+
+// 浮点型
+int floatType(){
+	float pi = 3.14;
+	cout<<"pi ="<<pi<<endl;
+	return 0;
+}
+int doubleType()       
+int main() {
+	floatType();
+}
+
+// 二进制
+
+
+// 哈希表
+
+// 图
+
+// 树
+// 二叉树
+// 二叉树：前序、中序、后续遍历
+
+// 字符串的操作
+
+// 递归
+
+// 时间复杂度
+
+// algorithm
 
 // sort 排序
 // 插入排序
@@ -26,6 +85,4 @@ int forSum(){
 // 堆排序
 // 归并排序
 
-int main() {
-    forSum();
-}
+// 分治算法
