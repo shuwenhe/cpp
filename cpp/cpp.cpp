28ed837837e0a4db7b6d1fbe112353a57442717a
Shuwen He <1201220707@pku.edu.cn>
2023-06-19 11:52:26 +0800

    fix: sum

--- cpp/cpp.cpp
+++ cpp/cpp.cpp
@@ -153,7 +153,16 @@ int typeTest()  {
     return 0;  
 }
 
+// 局部变量
+int sum(){
+	int a,b,sum; // 局部变量声明
+	a = 1,b = 2; // 实际初始化
+	sum = a + b;
+	cout<<"sum = "<<sum<<endl;
+	return 0;
+}
+
 int main() {
-	typeTest();
+	sum();
 }
 
