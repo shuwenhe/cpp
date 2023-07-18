2677c8c672aff98780e946ff0da8331e4abf0155
Shuwen He <1201220707@pku.edu.cn>
2023-07-18 11:58:52 +0800

    feat: array

---
+++ code/array/csp-j1-2021-two-3/csp-j1-2021-two-3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int n = 100000;
+int N = n+1;
+
+int m;
+int a[N],b[N],c[N],d[N];
+int f[N],g[N];
+
+void init(){
+	f[1] = g[1] = 1;
+}
+
+int main(){
+	init();
+
+	int x
+	return 0;
+}
