ed9bbcc2f8c92c5f80362c5868bd541c4efabbc5
Shuwen He <1201220707@pku.edu.cn>
2023-07-21 13:53:54 +0800

    feat: sizeof variable

---
+++ code/operator/sizeof-variable/sizeof.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"sizeof(char) = "<<sizeof(char)<<" bytes"<<endl;
+	cout<<"sizeof(int) = "<<sizeof(int)<<" bytes"<<endl;
+	cout<<"sizeof(long) = "<<sizeof(long)<<" bytes"<<endl;
+	cout<<"sizeof(long long) = "<<sizeof(long long)<<" bytes"<<endl;
+	cout<<"sizeof(float) = "<<sizeof(float)<<" bytes"<<endl;
+	cout<<"sizeof(double) = "<<sizeof(double)<<" bytes"<<endl;
+}
---
+++ code/operator/sizeof/sizeof.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int arr[] = {60,34,25,12,22,11,90};
+	int size = sizeof(arr);
+	int size2 = sizeof(arr[0]);
+	cout<<size<<endl;
+	cout<<size2<<endl;
+}
