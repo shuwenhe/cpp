d041902bc2db0cc5c6a76ff23219b547dc6b44b4
Shuwen He <1201220707@pku.edu.cn>
2023-07-21 09:53:27 +0800

    feat: string

--- code/string/string.cpp
+++ code/string/string.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main(){
-	string name = "My name is Richard!";
-	cout<<name<<endl;
-	cout<<"name length is "<<name.length()<<endl;
+	string str = "01010000";
+	int count = 0;
+	for(int i = 0;i < str.length();i++ ){
+		if(str[i] == '1'){
+			count++;
+		}
+	}
+	cout<<count<<endl;
 	return 0;
 }
