b96bbd171f265072f7a3ff0e08bb780758ae600e
Shuwen He <1201220707@pku.edu.cn>
2023-07-21 13:20:12 +0800

    feat: sizeof

--- code/array/array.cpp
+++ code/array/array.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 
 int main(){
-	int nums[3] = {1,2,3};
-	for(int i = 0; i < sizeof(nums)/sizeof(nums[0]);i++){
-		cout<<nums[i]<<endl;
+	int arr[] ={64, 34, 25, 12, 22, 11, 90};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	for(int i = 0;i<size;i++){
+		cout<<arr[i]<<" ";
 	}
+	cout<<endl;
+
 	return 0;
 }
