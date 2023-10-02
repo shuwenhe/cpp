c885938d235f118a5abdfd42a90a82e0386439a0
Shuwen He <1201220707@pku.edu.cn>
2023-10-02 11:20:08 +0800

    feat:float

---
+++ src/char/char-small-big.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	char c;
+	c = 'a'; // a -> A
+	c -= 32;
+	cout<<c<<'\n';
+}
---
+++ src/float/float-to-int.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	float i = 3.5;
+	cout<<"i = "<<i<<'\n';
+	int j = (int)i;
+	cout<<"j = "<<j<<'\n';
+}
